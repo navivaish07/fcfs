a3b3af30f900067cb3597ada5dbae41458f0aab0
Vaishnavi Dhanwate <143507244+navivaish07@users.noreply.github.com>
2026-05-04 23:19:42 +0530

    Implement FCFS scheduling algorithm in C

---
+++ fcfs.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int bt[20], wt[20], tat[20];
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        printf("Enter Burst Time for P%d: ", i + 1);
+        scanf("%d", &bt[i]);
+    }
+
+    // First process waiting time is 0
+    wt[0] = 0;
+
+    // Calculate waiting time
+    for(i = 1; i < n; i++) {
+        wt[i] = wt[i - 1] + bt[i - 1];
+    }
+
+    // Calculate turnaround time
+    for(i = 0; i < n; i++) {
+        tat[i] = wt[i] + bt[i];
+    }
+
+    // Display result
+    printf("\nProcess\tBT\tWT\tTAT\n");
+
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+    }
+
+    return 0;
+}
